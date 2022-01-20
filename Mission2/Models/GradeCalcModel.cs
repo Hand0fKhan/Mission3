05cd9d17bcdcab240c471db08216f3ff43165f9b
Hand0fKhan <americandragon0627@gmail.com>
2022-01-19 20:40:11 -0700

    Add project files.

---
+++ Mission2/Models/GradeCalcModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission3.Models
+{
+    public class GradeCalcModel
+    {
+        [Required]
+        public int Assignments { get; set;  }
+        [Required]
+        public int Projects { get; set; }
+        [Required]
+        public int Quiz { get; set; }
+        [Required]
+        public int Test { get; set; }
+        [Required]
+        public int Intex { get; set; }
+    }
+}
+
---
+++ Mission2/wwwroot/js/GradeCalc.js
@@ -0,0 +1,24 @@
+﻿$("#calcBtn").click(function () {
+    assignment = ($("#assign").val() * .55);
+    projects = ($("#groupProj").val() * .05);
+    quizzes = ($("#quiz").val() *.1);
+    exams = ($("#test").val() * .2);
+    intex = ($("#intex").val() * .1);
+
+    sum = assignment + projects + quizzes + exams + intex;
+    grade = 'Undetermined'
+    if (sum < 68)
+        grade = 'F'
+    else if (sum == 69)
+        grade = 'F, but nice'
+    else if (sum >= 70 && sum < 80)
+        grade = 'C'
+    else if (sum >= 80 && sum < 90)
+        grade = 'B'
+    else if (sum >= 90 && sum < 100)
+        grade = 'A'
+    else if (sum >= 100)
+        grade = 'A+'
+
+    alert("Estimated Grade: " + sum + ' (' + grade + ')')
+})
